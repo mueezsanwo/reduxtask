4209a1889f5e29ee66eca2af29e86abb6dd17abb
codeCampWithSifat <sayedhossainsifat300@.com>
2022-07-04 01:08:28 +0600

    full done

--- src/Components/Count.js
+++ src/Components/Count.js
@@ -1,15 +1,28 @@
 import React from 'react'
-
+import {useSelector, useDispatch} from 'react-redux';
+import { decrementCounter, incrementCounter, resetCounter } from './Services/actions/counterActions';
 const Count = () => {
-    
+    const counts = useSelector(state => state.count);
+    // console.log(counts);
+    const dispatch = useDispatch()
     const handleIncrement = () => {
-        console.log("button Clicked")
+        dispatch(incrementCounter())
+    };
+
+    const handleDecrement = () => {
+      dispatch(decrementCounter())
+    };
+
+    const handleReset = () => {
+      dispatch(resetCounter());
     }
   return (
     <div>
       <h3>Developer Counter App</h3>
-      <h3>Count</h3>
+      <h3>Count {counts}</h3>
       <button onClick={handleIncrement}>Increment </button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   )
 }
@@ -21,5 +34,6 @@ export default Count
 // reducers - increment -> Count -> count + 1 ;
 // reducers => decrement => Count => count - 1 ;
 // reducers => reset => count => 0;
-// store => 
+// store => to create store
+// providing store/use the store
 
